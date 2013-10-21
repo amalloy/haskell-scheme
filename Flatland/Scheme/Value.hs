0bf40f4d956f18fe68377b0dfa7414c1dc530b1a
Alan Malloy <alan@malloys.org>
2013-10-21 02:43:13 -0700

    Enough value definitions for the reader

--- Flatland/Scheme/Value.hs
+++ Flatland/Scheme/Value.hs
@@ -1,3 +1,18 @@
 module Flatland.Scheme.Value where
 
-data Value = Nil | Cons Value Value | Lambda (Value -> Value) | Symbol
+import Data.List
+
+type Env = [(String, Value)]
+
+data Value = Nil | Cons {car::Value, cdr::Value}
+           | Lambda (Value -> Value) Value
+           | Symbol String
+
+instance Show Value where
+  show Nil = "()"
+  show (Symbol s) = s
+  show (Lambda f source) = show source
+  show c@(Cons a d) = "(" ++ (intercalate " " $ showList c) ++ ")"
+    where showList (Cons a Nil) = [(show a)]
+          showList (Cons a d@(Cons _ _)) = show a : showList d
+          showList (Cons a d) = show a : "." : [show d]
