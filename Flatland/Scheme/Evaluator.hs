6c0b9c147a9d14492a2eef7c5537bc8775beb3fe
Alan Malloy <alan@malloys.org>
2013-11-07 13:01:59 -0800

    Make it easier to produce Exception values

--- Flatland/Scheme/Evaluator.hs
+++ Flatland/Scheme/Evaluator.hs
@@ -4,8 +4,14 @@ import Data.List
 import Control.Monad.Error
 import Flatland.Scheme.Types
 
-arityException :: Int -> Int -> SchemeException
-arityException expected actual = RuntimeException $ ArityException expected actual
+runtimeException :: (a -> b -> RuntimeException) -> a -> b -> Result
+runtimeException f a b = Left . RuntimeException $ f a b
+
+arityException :: Int -> Int -> Result
+arityException = runtimeException ArityException
+
+typeError :: [SchemeType] -> SchemeType -> Result
+typeError = runtimeException TypeError
 
 asList :: String -> Value -> Either SchemeException [Value]
 asList context Nil = return []
@@ -19,15 +25,15 @@ lookupEnv :: Env -> Value -> Result
 lookupEnv (Cons (Cons (Symbol a) v) e) s@(Symbol b) | a == b = Right v
                                                     | otherwise = lookupEnv e s
 lookupEnv Nil s = Left $ CompilerException $ UnresolvedSymbol s
-lookupEnv (Cons (Cons x _) e) s = Left $ RuntimeException $ TypeError [SymbolType] (typeOf x)
-lookupEnv (Cons x e) s = Left $ RuntimeException $ TypeError [ConsType] (typeOf x)
-lookupEnv x s = Left $ RuntimeException $ TypeError [ConsType] (typeOf x)
+lookupEnv (Cons (Cons x _) e) s = typeError [SymbolType] (typeOf x)
+lookupEnv (Cons x e) s = typeError [ConsType] (typeOf x)
+lookupEnv x s = typeError [ConsType] (typeOf x)
 
 eval :: Env -> Value -> Result
 eval _ Nil = return Nil
 eval e (Symbol "*env*") = return e
 eval e s@(Symbol _) = lookupEnv e s
-eval e (Lambda f source) = Left $ RuntimeException $ TypeError [NilType, ConsType, SymbolType] LambdaType
+eval e (Lambda f source) = typeError [NilType, ConsType, SymbolType] LambdaType
 eval e c@(Cons a d) =
   case a of
     (Symbol "quote") -> return $ car d
@@ -38,19 +44,19 @@ eval e c@(Cons a d) =
         [test, t, f] -> do
           v <- eval e test
           eval e (if v == Nil then f else t)
-        otherwise -> Left $ arityException 3 (length body)
+        otherwise -> arityException 3 (length body)
     otherwise -> do
       argList <- (asList "arguments to lambda" c)
       (f:args) <- forM argList (eval e)
       -- note we're not handling [] at all - that case is impossible, because we already know we have a Cons
       case f of
         (Lambda f source) -> f args
-        otherwise -> Left $ RuntimeException $ TypeError [LambdaType] (typeOf f)
+        otherwise -> typeError [LambdaType] (typeOf f)
 
 withEnv :: [Value] -> [Value] -> Env -> Either SchemeException Env
 withEnv params args e | (paramCount == argCount) = return $ foldr Cons e $
                                                    zipWith Cons params args
-                      | otherwise = Left $ arityException paramCount argCount
+                      | otherwise = arityException paramCount argCount
   where paramCount = length params
         argCount = length args
 
@@ -68,12 +74,12 @@ nativeCode = asCons $ map Symbol ["native", "code"]
 schemeFn :: (Value -> Result) -> Value
 schemeFn f = Lambda g nativeCode
   where g [x] = f x
-        g args = Left $ arityException 1 (length args)
+        g args = arityException 1 (length args)
 
 schemeFn2 :: (Value -> Value -> Result) -> Value
 schemeFn2 f = Lambda g nativeCode
   where g [x, y] = f x y
-        g args = Left $ arityException 2 (length args)
+        g args = arityException 2 (length args)
 
 sEq :: Value
 sEq = schemeFn2 eq
@@ -87,7 +93,7 @@ sCons = schemeFn2 cons
 consFn :: (Value -> Value) -> Value
 consFn f = schemeFn g
   where g c@(Cons _ _) = return (f c)
-        g x = Left $ RuntimeException $ TypeError [ConsType] (typeOf x)
+        g x = typeError [ConsType] (typeOf x)
 
 sCar :: Value
 sCar = consFn car
